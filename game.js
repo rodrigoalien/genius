c786839444f711d3fa74dcb142053961218be7cc
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2020-04-23 22:36:02 -0300

    Buttons have sound and flash.

--- game.js
+++ game.js
@@ -3,34 +3,53 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+//var start = false;
 
-$(document).on("keydown", function(){
+$(document).on("keydown", function() {
+  $("#level-title").text("Level " + level);
+  //start = true;
   nextSequence();
   $(document).off();
 });
 
 function nextSequence() {
 
-  //document.removeEventListener("keydown", nextSequence);
+  level++;
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  playSound(randomChosenColour);
 
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
 
   /* for( var i = 0; i < 4; i++) {
     $("." + buttonColours[i]).click( function() { handler(buttonColours[i]); });
   } */
 
-  $(".btn").click( function() {
+  $(".btn").click(function() {
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  });
+}
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+
+}
+
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed").delay(100).queue( function() {
+    $(this).removeClass("pressed").dequeue();
   });
 
 }
